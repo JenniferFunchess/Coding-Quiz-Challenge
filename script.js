fe9c1f17082a81ad8daa5e0ab8d5830c163cf26b
Jennifer Funchess <jfunchess@ymail.com>
2020-09-29 01:16:06 -0400

    set up timer interval in js

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
-var = document.getElementbyId("")
+var countDown = document.getElementbyId("timer");
 
+var quizQuestions = [
+  {
+    question: "The condition in an if/else statmennt is enclosed within ____.",
+    choices: ["Quotes", "Curly brackets", "parentheses", "square brackets"],
+    answer: "Curly brackets",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ______.",
+    choices: [
+      "Numbers and strings",
+      "Other arrays",
+      "booleans",
+      "all are true",
+    ],
+    answer: "all of true",
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScripts", "Terminal/bash", "for loops", "console.log"],
+    answer: "console.log",
+  },
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["Strings", "booleans", "alerts", "numbers"],
+    answer: "booleans",
+  },
+  {
+    question:
+      "String values must ben enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "parenthesis",
+  },
+];
 
-var questions = [
-    {
-        question: "The condition in an if/else statmennt is enclosed within ____.",
-        choices: ["Quotes","Curly brackets","parentheses","square brackets"],
-        answer: "Curly brackets",
-    },
-    {
-        question: "Arrays in JavaScript can be used to store ______.",
-        choices: ["Numbers and strings","Other arrays","booleans","all are true"],
-        answer: "all of true",
-    },
-    {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choices: ["JavaScripts","Terminal/bash","for loops","console.log"],
-        answer: "console.log",
-    },
-    {
-        question:"Commonly used data types DO NOT include:",
-        choices:["Strings", "booleans","alerts","numbers"],
-        answer:"booleans",
-    },
-    {
-        question: "String values must ben enclosed within ____ when being assigned to variables.",
-        choices: ["commas","curly brackets","quotes","parentheses"],
-        answer:"parenthesis",
-    },
-]
+var timeLeft = 75;
+var interval;
+var questionsIndex = 0;
+
+window.onload = function () {
+  timerDisplay.textContent = "Time:0";
+};
+function startTimer() {
+  countDown = setInterval(function () {
+    timeLeft--;
+    countDown.textContent = timeLeft;
+    if (timeLeft <= 0) {
+      showScore();
+    }
+  }, 1000);
+}
+
+function startQuiz() {
+  mainPage.style.display = "none";
+  renderQuestions(questionIndex);
+}
