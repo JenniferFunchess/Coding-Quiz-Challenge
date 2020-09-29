2c558c61602923a9d4a3e414b3f2e7ff0642a2fd
Jennifer Funchess <jfunchess@ymail.com>
2020-09-28 23:37:21 -0400

    added questions in javascript

---
+++ script.js
@@ -0,0 +1,30 @@
+var = document.getElementbyId("")
+
+
+var questions = [
+    {
+        question: "The condition in an if/else statmennt is enclosed within ____.",
+        choices: ["Quotes","Curly brackets","parentheses","square brackets"],
+        answer: "Curly brackets",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ______.",
+        choices: ["Numbers and strings","Other arrays","booleans","all are true"],
+        answer: "all of true",
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScripts","Terminal/bash","for loops","console.log"],
+        answer: "console.log",
+    },
+    {
+        question:"Commonly used data types DO NOT include:",
+        choices:["Strings", "booleans","alerts","numbers"],
+        answer:"booleans",
+    },
+    {
+        question: "String values must ben enclosed within ____ when being assigned to variables.",
+        choices: ["commas","curly brackets","quotes","parentheses"],
+        answer:"parenthesis",
+    },
+]
